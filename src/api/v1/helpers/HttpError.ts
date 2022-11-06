bbcf24656aa3b8d6d017f7d5665d1a2ad7b02d4c
fatiudeen <shehufatiudeen@gmail.com>
2022-11-06 07:37:54 +0100

    helper methods

---
+++ src/api/v1/helpers/HttpError.ts
@@ -0,0 +1,10 @@
+export default class HttpError extends Error {
+  statusCode: number;
+  data: object | null;
+
+  constructor(message: string, statusCode?: number, data?: object) {
+    super(message);
+    this.statusCode = statusCode === undefined ? 500 : statusCode;
+    this.data = data === undefined ? null : data;
+  }
+}
---
+++ src/api/v1/helpers/db.ts
@@ -0,0 +1,4 @@
+import knex from 'knex';
+import DB_CONFIG from '@knexfile';
+
+export default knex(DB_CONFIG);
